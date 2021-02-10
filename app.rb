d992a9c98cfac95c393108e34cfba6e0c75cbb0f
Kellen <kellenbmay@gmail.com>
2021-02-10 13:39:12 -0500

    commit tree da0b0de19bde7e14b85bacc1c7a6a128adeb9918

--- app.rb
+++ app.rb
@@ -14,5 +14,28 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
+  get "/hello/:name" do
+    @user_name = params[:name]
+    "Hello #{@user_name}!"
+  end
 
+  get "/goodbye/:name" do
+  	@name = params[:name]
+  	"Goodbye, #{@name}."
+  end
+
+  get "/multiply/:num1/:num2" do
+  	@product = params[:num1].to_i * params[:num2].to_i
+  	"#{@product}"
+  end
+
+  get "/goodbye/:name" do
+    @name = params[:name]
+    "Goodbye, #{@name}."
+  end
+
+  get "/multiply/:num1/:num2" do
+    @product = params[:num1].to_i * params[:num2].to_i
+    "#{@product}"
+  end
 end
